16c1a34514b51b6da87f47c74a274cde652c2941
nogs <90790271+ronskie69@users.noreply.github.com>
2023-11-22 10:49:12 +0800

    Updates

---
+++ precache-manifest.bdf6d82af583d5ad83767748bb1de26b.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "269430f08a6eb0e6f2c02980f5e8c69c",
+    "url": "/my-music/index.html"
+  },
+  {
+    "revision": "188aad89d1147e8332ad",
+    "url": "/my-music/static/css/main.1eb46796.chunk.css"
+  },
+  {
+    "revision": "d07ae441a1a310902e34",
+    "url": "/my-music/static/js/2.e3942cbd.chunk.js"
+  },
+  {
+    "revision": "c1bdf306936ddd82567a345834b1f9d5",
+    "url": "/my-music/static/js/2.e3942cbd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "188aad89d1147e8332ad",
+    "url": "/my-music/static/js/main.35c88a25.chunk.js"
+  },
+  {
+    "revision": "8a81a717a93e8cfa4bc0",
+    "url": "/my-music/static/js/runtime-main.5fdc411d.js"
+  },
+  {
+    "revision": "0caf573096e9ba53824618930b2782d8",
+    "url": "/my-music/static/media/bgperson.0caf5730.jpg"
+  },
+  {
+    "revision": "5849c6a47d47acab8a170773aaab70cd",
+    "url": "/my-music/static/media/controller.5849c6a4.png"
+  },
+  {
+    "revision": "ba1d2a3ca7114781c24013e3fcd2b207",
+    "url": "/my-music/static/media/disk2.ba1d2a3c.png"
+  },
+  {
+    "revision": "085b1dd8427dbeff10bd55410915a3f6",
+    "url": "/my-music/static/media/fa-brands-400.085b1dd8.ttf"
+  },
+  {
+    "revision": "0fabb6606be4c45acfeedd115d0caca4",
+    "url": "/my-music/static/media/fa-brands-400.0fabb660.eot"
+  },
+  {
+    "revision": "cac68c831145804808381a7032fdc7c2",
+    "url": "/my-music/static/media/fa-brands-400.cac68c83.woff2"
+  },
+  {
+    "revision": "ccfdb9dc442be0c629d331e94497428b",
+    "url": "/my-music/static/media/fa-brands-400.ccfdb9dc.svg"
+  },
+  {
+    "revision": "dc0bd022735ed218df547742a1b2f172",
+    "url": "/my-music/static/media/fa-brands-400.dc0bd022.woff"
+  },
+  {
+    "revision": "05b53beb21e3ef13d28244545977152d",
+    "url": "/my-music/static/media/fa-regular-400.05b53beb.woff"
+  },
+  {
+    "revision": "1a78af4105d4d56e6c34f76dc70bf1bc",
+    "url": "/my-music/static/media/fa-regular-400.1a78af41.ttf"
+  },
+  {
+    "revision": "3a3398a6ef60fc64eacf45665958342e",
+    "url": "/my-music/static/media/fa-regular-400.3a3398a6.woff2"
+  },
+  {
+    "revision": "ad3a7c0d77e09602f4ab73db3660ffd8",
+    "url": "/my-music/static/media/fa-regular-400.ad3a7c0d.eot"
+  },
+  {
+    "revision": "e75dfd904d366a2560c63c23cfc98ef8",
+    "url": "/my-music/static/media/fa-regular-400.e75dfd90.svg"
+  },
+  {
+    "revision": "03ba7cb710104df27f1c9c46d64bee4e",
+    "url": "/my-music/static/media/fa-solid-900.03ba7cb7.svg"
+  },
+  {
+    "revision": "781e85bb50c8e8301c30de56b31b1f04",
+    "url": "/my-music/static/media/fa-solid-900.781e85bb.ttf"
+  },
+  {
+    "revision": "89bd2e38475e441a5cd70f663f921d61",
+    "url": "/my-music/static/media/fa-solid-900.89bd2e38.eot"
+  },
+  {
+    "revision": "c500da19d776384ba69573ae6fe274e7",
+    "url": "/my-music/static/media/fa-solid-900.c500da19.woff2"
+  },
+  {
+    "revision": "ee09ad7553b8ad3d81150d609d5341a0",
+    "url": "/my-music/static/media/fa-solid-900.ee09ad75.woff"
+  },
+  {
+    "revision": "57e937e4eef39e324bd4f26745053687",
+    "url": "/my-music/static/media/titillium.57e937e4.ttf"
+  }
+]);
